4fdf86d2f4c53bb6c8be58cd7944fe4118cff0ef
Stuart-Yee <ki6fzb@gmail.com>
2022-04-18 21:29:04 -0700

    Hello world in Java in German...

---
+++ SomeBasics/src/HalloWelt/HalloWelt.java
@@ -0,0 +1,25 @@
+package HalloWelt;
+
+import java.util.Scanner;
+
+public class HalloWelt {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Sprechen Sie Deutsch?");
+		String response = scan.next() + scan.nextLine();
+		if (response.toLowerCase().equals("ja")) {
+			System.out.println("Sehr gut, wie heissen Sie?");
+		} else {
+			System.out.println("Perhaps English then? What's your name?");
+		}
+		String name = scan.next();
+		if (response.toLowerCase().equals("ja")) {
+			System.out.printf("Freut mich, %s!%n", name);
+		} else {
+			System.out.printf("Nice to meet you, %s. Try learning German sometime.%n", name);
+		}
+
+	}
+
+}
